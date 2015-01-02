b9e3419e49bc805dc564a5cf78911f7049bf5745
Stephan Wentz <swentz@brainbits.net>
2015-01-02 16:41:53 +0100

    js cleanups for puli usage

--- Resources/scripts/store/Portlet.js
+++ Resources/scripts/store/Portlet.js
@@ -1,4 +1,4 @@
-Ext.namespace('Phlexible.dashboard');
+Ext.ns('Phlexible.dashboard.store');
 
 Phlexible.dashboard.store.Portlet = new Ext.data.SimpleStore({
     fields: Phlexible.dashboard.model.Portlet
